d6953183540aae36059035a0b776617b57af4a97
Jishnu P.S <jishnu82003@gmail.com>
2020-08-01 08:00:57 +0530

    Home page

--- Site/js/script.js
+++ Site/js/script.js
@@ -1,1 +1,7 @@
-var x = "Hello world";
+var quotes = ['The Road Not taken', 'fdjsnfjksdbfjsbdf', 'fkdnsjkfbsfsdf',
+'djkfbasdjkfbjsdfbj', 'fsdjbfwehrirewrhw', 'feirhqeiwbfebf']
+
+function newQuote() {
+    var randomNumber = Math.floor(Math.random() * (quotes.length));
+    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+}
